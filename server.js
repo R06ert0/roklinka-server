64b31ea4e2e0f174f5af92dc03b2c580c663eea0
R06ert <dominguezbert@gmail.com>
2021-09-19 12:52:56 +0200

    PORT update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const PORT = process.env.PORT || 4001;
 
 app.use(express.json())
 app.use(cors({}))
@@ -16,4 +17,4 @@ const occupiedRouter = require('./routes/occupied')
 
 app.use('/occupied', occupiedRouter)
 
-app.listen(4000)
+app.listen(PORT)
